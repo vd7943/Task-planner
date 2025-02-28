cece8ecbb30d0ad78fb1b886f2b4a17d5e36207f
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-02-28 17:14:55 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -9,10 +9,12 @@ export const sendEmail = async ({ email, userType, taskName }) => {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     service: process.env.SMTP_SERVICE,
+     secure: true,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
     },
+     debug: true,
   });
 
   const template = await EmailTemplate.findOne({ userType });
